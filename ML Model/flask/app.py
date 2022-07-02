fe08969e06ec1caf7f216fa9269a86101df9aea9
Eshaan Bhardwaj <55873406+Eshaan-B@users.noreply.github.com>
2022-07-03 00:31:58 +0530

    chore: trying to send file to model

--- ML Model/flask/app.py
+++ ML Model/flask/app.py
@@ -2,10 +2,13 @@ from msilib.schema import File
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 import os
+import pickle
 
 app = Flask('__name__')
 app.config['SECRET_KEY']='password'
 app.config['UPLOAD_FOLDER']='static/files'
+model = pickle.load(open('../model.pkl', 'rb'))
+
 
 @app.route('/',methods=['GET','POST'])
 def home():
@@ -14,9 +17,11 @@ def home():
 def upload():
     if request.method == 'POST':
         file=request.files['file']
+        print(file)
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return 'File uploaded successfully'
+        prediction = model.predict(file.read());
+        return prediction
     else:
         return 'Get request to /upload'
 
