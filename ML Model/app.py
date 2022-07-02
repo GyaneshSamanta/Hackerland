40e2caf566b38c5132bd601e0c263e7ee1c24461
Eshaan Bhardwaj <55873406+Eshaan-B@users.noreply.github.com>
2022-07-02 18:54:37 +0530

    File upload completed

--- ML Model/app.py
+++ ML Model/app.py
@@ -1,13 +1,25 @@
-from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import FileField
+from msilib.schema import File
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import os
+
 app = Flask('__name__')
+app.config['SECRET_KEY']='password'
+app.config['UPLOAD_FOLDER']='static/files'
 
-
-@app.route('/')
-@app.route('/home')
+@app.route('/',methods=['GET','POST'])
 def home():
-    return render_template('index.html')
+    return 'Server is running'
+@app.route('/upload',methods=['GET','POST'])
+def upload():
+    if request.method == 'POST':
+        file=request.files['file']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return 'File uploaded successfully'
+    else:
+        return 'Get request to /upload'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
